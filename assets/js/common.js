9b8ca1c21f7a06dd2b698ad7fca410acff9e340a
duongspm <tranvanduong10a22016bb@gmail.com>
2023-07-04 11:12:15 +0700

    #Day 3: Best seller

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,14 +4,13 @@ function parts(rootDir, File) {
         url: rootDir + "library/" + File,
         cache: false,
         async: false,
-        dataType: 'html',
+        dataType: "html",
         success: function (html) {
             html = html.replace(/\{\$root\}/g, rootDir);
             document.write(html);
-        }
+        },
     });
 }
-
 
 $(function () {
     var header;
@@ -20,8 +19,8 @@ $(function () {
     } else {
         header = $("header").outerHeight();
     }
-    $('main').css('margin-top', header);
-})
+    $("main").css("margin-top", header);
+});
 $(window).resize(function () {
     var header;
     if ($(window).width() < 768) {
@@ -29,78 +28,72 @@ $(window).resize(function () {
     } else {
         header = $("header").outerHeight();
     }
-    $('main').css('margin-top', header);
+    $("main").css("margin-top", header);
 });
-
-
 
 /* SETTING for Smart Phone Menu Display */
 $(function () {
     spMenuDisplay();
 });
 function spMenuDisplay() {
-    const menu = $('.gNav');
-    const btn = $('#navBtn');
-    const header = $('#header');
-    btn.on('click', function (e) {
+    const menu = $(".gNav");
+    const btn = $("#navBtn");
+    const header = $("#header");
+    btn.on("click", function (e) {
         e.preventDefault();
-        menu.toggleClass('active');
-        btn.toggleClass('active');
-        header.toggleClass('active');
-        $('html,body').toggleClass('noScroll');
+        menu.toggleClass("active");
+        btn.toggleClass("active");
+        header.toggleClass("active");
+        $("html,body").toggleClass("noScroll");
     });
 }
-
-
 
 // Set header - footer active menu
 $(function () {
     var url = window.location.pathname,
-        urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
-    if (urlRegExp.test('/')) {
-        $('.gNav > ul > li:first-of-type a').addClass('active');
+        urlRegExp = new RegExp(url.replace(/\/$/, "") + "$");
+    if (urlRegExp.test("/")) {
+        $(".gNav > ul > li:first-of-type a").addClass("active");
     } else {
-        $('header .gNav > ul > li > a').each(function () {
-            if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-                $(this).addClass('active');
+        $("header .gNav > ul > li > a").each(function () {
+            if (urlRegExp.test(this.href.replace(/\/$/, ""))) {
+                $(this).addClass("active");
             }
         });
-        $('header .gNav > ul > li ul li a').each(function () {
-            if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-                $(this).addClass('active');
+        $("header .gNav > ul > li ul li a").each(function () {
+            if (urlRegExp.test(this.href.replace(/\/$/, ""))) {
+                $(this).addClass("active");
             }
         });
     }
-
 });
-
-
 
 // Pagetop
 $(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('#pagetop').fadeIn();
+            $("#pagetop").fadeIn();
         } else {
-            $('#pagetop').fadeOut();
+            $("#pagetop").fadeOut();
         }
     });
 
-    $('#pagetop').on('click', function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
+    $("#pagetop").on("click", function () {
+        $("body,html").animate(
+            {
+                scrollTop: 0,
+            },
+            400
+        );
         return false;
     });
 });
 
-$(function(){
-    $('.slider-slick').slick(
-        {
-            fade: true,
-        }
-    );
-})
+$(function () {
+    $(".slider-slick").slick({
+        fade: true,
+    });
+});
 // Menu
 $(document).ready(function () {
     $open = false;
@@ -123,5 +116,59 @@ $(document).ready(function () {
 
     $("#js-menu-btn").on("click", function () {
         menuMb();
+    });
+});
+
+$(function () {
+    $(".js-slickbs-for").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: ".js-slickbs-nav",
+        lazyLoad: 'progressive', 
+        infinite: true,
+        autoplay: false,
+        speed: 600,
+        cssEase: "linear",
+        rtl: false, //right to left
+        accessibility: true,
+        arrows: false,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        swipe: true,
+        focusOnSelect: false,
+        draggable: false,
+        useTransform: true,
+        
+        draggable: true,
+        verticalSwiping: false,
+        autoplaySpeed: 2000, 
+        speed: 1000, 
+        centerMode: false, 
+        dots: false, 
+        responsive: [
+        ]
+    });
+    $(".js-slickbs-nav").slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        vertical: true,
+        asNavFor: ".js-slickbs-for",
+        arrows: false,
+
+        lazyLoad: 'progressive', 
+        infinite: true, 
+        accessibility: true, 
+        vertical: true, 
+        verticalSwiping: true,
+        autoplay: true, 
+        autoplaySpeed: 2000, 
+        speed: 1000, 
+        focusOnSelect: true,
+		centerMode: false, 
+        dots: false, 
+        draggable: true, 
+        responsive: [
+		]
     });
 });
