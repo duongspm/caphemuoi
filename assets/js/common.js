213cb1a535c1bb75b2a909608e17b75561728bcc
duongspm <tranvanduong10a22016bb@gmail.com>
2023-07-04 17:00:21 +0700

    #Day6: update Story

--- assets/js/common.js
+++ assets/js/common.js
@@ -180,4 +180,20 @@ $(document).ready(function(){
 $(document).ready(function(){
     AOS.init(); 
 });
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        focusOnSelect: true
+    });
+});
 
